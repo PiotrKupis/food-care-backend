345e770c5b80329b4766ac2f8f741b26d0bb6ddd
Piotr Kupis <piotr.kupis42@gmail.com>
2021-10-20 09:13:49 +0200

    initial commit

---
+++ src/main/java/com/andromeda/foodcare/FoodCareApplication.java
@@ -0,0 +1,13 @@
+package com.andromeda.foodcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodCareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andromeda/foodcare/FoodCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andromeda.foodcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
