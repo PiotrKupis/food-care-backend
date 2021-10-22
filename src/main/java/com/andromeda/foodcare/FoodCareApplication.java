f0ef5168269a832b3b844d06086d026daa035e78
Michał Jankowski <215744@edu.p.lodz.pl>
2021-10-22 12:14:24 +0200

    add db

--- src/main/java/com/andromeda/foodcare/FoodCareApplication.java
+++ src/main/java/com/andromeda/foodcare/FoodCareApplication.java
@@ -2,9 +2,10 @@ package com.andromeda.foodcare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FoodCareApplication {
+public class FoodCareApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(FoodCareApplication.class, args);
