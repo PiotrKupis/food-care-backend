5b085ef145dc8c4526399e0faa9b3383c06eadfb
Piotr Kupis <piotr.kupis42@gmail.com>
2021-10-30 13:48:26 +0200

    add business entity

--- src/main/java/com/andromeda/foodcare/model/Business.java
+++ src/main/java/com/andromeda/foodcare/model/Business.java
@@ -1,6 +1,7 @@
 package com.andromeda.foodcare.model;
 
-import com.andromeda.foodcare.enums.UserRole;
+import com.andromeda.foodcare.enums.BusinessType;
+import java.time.LocalTime;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -17,21 +18,17 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-/**
- * Class responsible for keeping information about single user.
- */
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
-@ToString(exclude = "business")
-@Entity(name = "users")
-public class User {
+@Data
+@ToString
+@Entity(name = "business")
+public class Business {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,40 +36,36 @@ public class User {
     private Long id;
 
     @NotBlank
+    @Size(min = 3, max = 60)
     @Column(nullable = false)
     private String name;
 
-    @NotNull
-    @Enumerated(EnumType.STRING)
-    private UserRole role;
-
     @NotBlank
     @Size(min = 5, max = 200)
-    @Column(nullable = false)
-    private String password;
-
-    @NotBlank
-    @Size(min = 2, max = 60)
     @Email(regexp = ".+@.+\\..+")
     @Column(nullable = false)
     private String email;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(
+        name = "address_id",
+        referencedColumnName = "id")
+    private Address address;
 
     @NotBlank
     @Size(min = 6, max = 12)
     @Column(nullable = false, name = "phone_number", length = 12)
     private String phoneNumber;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(
-        name = "business_id",
-        referencedColumnName = "id")
-    private Business business;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private BusinessType businessType;
 
-    public User(String name, UserRole role, String password, String email, String phoneNumber) {
-        this.name = name;
-        this.role = role;
-        this.password = password;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-    }
+    @NotNull
+    @Column(name = "open_hour", columnDefinition = "TIME", nullable = false)
+    private LocalTime openHour;
+
+    @NotNull
+    @Column(name = "close_hour", columnDefinition = "TIME", nullable = false)
+    private LocalTime closeHour;
 }
