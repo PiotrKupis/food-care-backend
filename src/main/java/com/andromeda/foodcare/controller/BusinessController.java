f2738848894535323da46c2a7e3cb2bbaa0667aa
Piotr Kupis <piotr.kupis42@gmail.com>
2021-10-30 14:35:56 +0200

    add business controller

---
+++ src/main/java/com/andromeda/foodcare/controller/BusinessController.java
@@ -0,0 +1,20 @@
+package com.andromeda.foodcare.controller;
+
+import com.andromeda.controller.BusinessApi;
+import com.andromeda.dto.BusinessPayload;
+import com.andromeda.foodcare.service.BusinessService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+public class BusinessController implements BusinessApi {
+
+    private final BusinessService businessService;
+
+    @Override
+    public ResponseEntity<BusinessPayload> addBusiness(BusinessPayload businessPayload) {
+        return ResponseEntity.ok(businessService.addBusiness(businessPayload));
+    }
+}
